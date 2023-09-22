acacbfd691c28f46e562d27eaee61a1525682534
Conor Butler <git@conor.tech>
2023-09-22 20:06:16 +0100

    feat: add links to hero

--- src/components/hero/hero.test.tsx
+++ src/components/hero/hero.test.tsx
@@ -1,21 +1,37 @@
 import { render, screen } from "@testing-library/react";
-import Hero from "./hero";
+import Hero from ".";
 
+const link1 = {
+  iconUrl: "example.png",
+  href: "https://example.com",
+  text: "Click me",
+};
+
+const link2 = {
+  iconUrl: "another-example.png",
+  href: "https://another-example.com",
+  text: "No click me",
+};
 const requiredProps = {
   title: "Hi",
   subtitle: "Bye",
+  links: [link1, link2],
 };
 
 describe("Hero", () => {
-  it("renders a title as a <h1> tag", () => {
+  it("renders a title and subtitle", () => {
     render(<Hero {...requiredProps} />);
+
     expect(
       screen.getByRole("heading", { level: 1, name: requiredProps.title })
     ).toBeVisible();
+    expect(screen.getByText(requiredProps.subtitle)).toBeVisible();
   });
 
-  it("renders a subtitle as a <p> tag", () => {
+  it("renders each link", () => {
     render(<Hero {...requiredProps} />);
-    expect(screen.getByText(requiredProps.subtitle)).toBeVisible();
+
+    expect(screen.getByRole("link", { name: link1.text })).toBeVisible();
+    expect(screen.getByRole("link", { name: link2.text })).toBeVisible();
   });
 });
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,16 +1,25 @@
-import type { ReactNode } from "react";
-import "./hero.css";
+import type { IconLinkProps } from "../icon-link/icon-link";
+import IconLink from "../icon-link/icon-link";
+import styles from "./hero.module.css";
 
-type HeroProps = {
+interface HeroProps {
   title: string;
   subtitle: string;
-};
+  links: IconLinkProps[];
+}
 
-const Hero = ({ title, subtitle }: HeroProps) => {
+const Hero = ({ title, subtitle, links }: HeroProps) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+    <div className={styles.component}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.subtitle}>{subtitle}</p>
+      <ul className={styles.links}>
+        {links.map((linkProps) => (
+          <li className={styles.link} key={linkProps.text}>
+            <IconLink {...linkProps} key={linkProps.text} isNewTab />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
