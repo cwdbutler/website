a572af256a9a469635438190bc994d86e1312817
Conor Butler <git@conor.tech>
2024-01-16 21:56:16 +0000

    chore: remove unnecessary ternary

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -2,18 +2,16 @@ import { ListItem, type ListItemProps } from "../list-item/list-item";
 
 interface ListProps {
   title: string;
-  items?: ListItemProps[];
+  items: ListItemProps[];
 }
 
 const List = ({ title, items }: ListProps) => (
   <div className="px-5">
     <h3 className="font-semibold">{title}</h3>
-    <ul className="space-y-1">
-      {items
-        ? items.map((item, i) => {
-            return <ListItem key={i} {...item} />;
-          })
-        : null}
+    <ul className="grid auto-rows-fr">
+      {items.map((item, i) => {
+        return <ListItem key={i} {...item} />;
+      })}
     </ul>
   </div>
 );
