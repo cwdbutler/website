1162338f500fc617cfafd277f721cd5516404afb
Conor Butler <git@conor.tech>
2024-01-29 17:02:37 +0000

    feat: add robots.txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://conor.tech/",
+  integrations: [react(), tailwind(), robotsTxt()],
 });
