de5b427d10433cf3c5a0eecdee0198ced57b0784
Conor Butler <git@conor.tech>
2024-01-29 17:23:55 +0000

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://conor.tech/",
-  integrations: [react(), tailwind(), robotsTxt()],
+  integrations: [react(), tailwind(), sitemap(), robotsTxt()],
 });
